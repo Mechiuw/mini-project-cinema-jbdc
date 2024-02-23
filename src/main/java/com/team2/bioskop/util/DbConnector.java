7afec7046908aec9be9b55bc157058662c9f8944
JuanLiestanto <@liestantojuandwiky@gmail.com>
2024-02-23 13:15:22 +0700

    [UPDATE] project structure

---
+++ src/main/java/com/team2/bioskop/util/DbConnector.java
@@ -0,0 +1,21 @@
+package com.team2.bioskop.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnector {
+    public Connection connectToDb() throws SQLException {
+        final String JDBC_URL = "jdbc:postgresql://localhost:5432/db-bioskop";
+        final String USERNAME = System.getenv("DB_USER");
+        final String PASSWORD = System.getenv("DB_PASSWORD");
+        Connection conn = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+
+            if(conn != null){
+                System.out.println("Connect Success");
+            }else{
+                System.out.println("Connect Failed");
+            }
+            return conn;
+    }
+}
