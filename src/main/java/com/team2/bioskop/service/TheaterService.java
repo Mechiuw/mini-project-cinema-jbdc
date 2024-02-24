765be61f50028aaa205db7578d8e94bb7748a3cb
bharaa <bharaa@Bharas-MacBook-Air.local>
2024-02-24 14:51:28 +0700

    update theaterservice

--- src/main/java/com/team2/bioskop/service/TheaterService.java
+++ src/main/java/com/team2/bioskop/service/TheaterService.java
@@ -1,9 +1,3 @@
 package com.team2.bioskop.service;
-
 public interface TheaterService {
-    static void addTheater() {}
-    static void readTheater() {}
-
-    static void updateTheater() {}
-    static void deleteTheater(){}
 }
