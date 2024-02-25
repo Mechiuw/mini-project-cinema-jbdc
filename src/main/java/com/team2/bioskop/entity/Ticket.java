0cff368f5ab5b4b75d8deec7849f2c549d2d691a
JuanLiestanto <@liestantojuandwiky@gmail.com>
2024-02-25 15:28:05 +0700

    [IMPROVE] Improve feature/ticket

--- src/main/java/com/team2/bioskop/entity/Ticket.java
+++ src/main/java/com/team2/bioskop/entity/Ticket.java
@@ -1,2 +1,43 @@
-package com.team2.bioskop.entity;public class Ticket {
+package com.team2.bioskop.entity;
+
+public class Ticket {
+    int customer_id;
+    int seat_id;
+    String seatNumber;
+    String theaterNumber;
+
+    public Ticket() {
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public String getTheaterNumber() {
+        return theaterNumber;
+    }
+
+    public void setTheaterNumber(String theaterNumber) {
+        this.theaterNumber = theaterNumber;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public int getSeat_id() {
+        return seat_id;
+    }
+
+    public void setSeat_id(int seat_id) {
+        this.seat_id = seat_id;
+    }
 }
